c7c4e99138b5fa4e4110af04f853207ad235f8d3
bateman001 <49342979+bateman001@users.noreply.github.com>
2022-06-14 14:49:56 -0600

    changed port and host options

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const cron = require("node-cron");
 const shell = require("shelljs");
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
+const host = "0.0.0.0";
 
 cron.schedule("*/30 * * * *", () => {
     console.log("running every 5 minutes");
     shell.exec("npm run first");
 });
 
-app.listen(PORT, error => {
+app.listen(port, host, error => {
     if (!error) console.log("Server is Successfully Running, and App is listening on port " + PORT);
     else console.log("Error occurred, server can't start", error);
 });
