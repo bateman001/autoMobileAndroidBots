e9084986d1eb8f2fe03941a3b3c1f92aa2aae4f6
bateman001 <49342979+bateman001@users.noreply.github.com>
2022-06-14 14:54:00 -0600

    fixed port bug

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ cron.schedule("*/30 * * * *", () => {
 });
 
 app.listen(port, host, error => {
-    if (!error) console.log("Server is Successfully Running, and App is listening on port " + PORT);
+    if (!error) console.log("Server is Successfully Running, and App is listening on port " + port);
     else console.log("Error occurred, server can't start", error);
 });
 
